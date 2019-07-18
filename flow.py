6fe6c0a844f02d2417ad5cc6696716f88bf05a7b
octavdragoi <octavdragoi@college.harvard.edu>
2019-07-18 20:40:34 +0200

    fixed bug with rotating pictures

--- flow.py
+++ flow.py
@@ -8,11 +8,25 @@ import os
 import sys
 import time
 from PIL import Image
+import functools
 
 # parameters for the global script
 SLEEPTIME = 2 # seconds
 APPROVED_EXTENSIONS = [".jpg", ".jpeg"]
 STANDARD_IMG_SIZE = (1440, 1920)
+
+exif_orientation_tag = 0x0112
+exif_transpose_sequences = [                   # Val  0th row  0th col
+    [],                                        #  0    (reserved)
+    [],                                        #  1   top      left
+    [Image.FLIP_LEFT_RIGHT],                   #  2   top      right
+    [Image.ROTATE_180],                        #  3   bottom   right
+    [Image.FLIP_TOP_BOTTOM],                   #  4   bottom   left
+    [Image.FLIP_LEFT_RIGHT, Image.ROTATE_90],  #  5   left     top
+    [Image.ROTATE_270],                        #  6   right    top
+    [Image.FLIP_TOP_BOTTOM, Image.ROTATE_90],  #  7   right    bottom
+    [Image.ROTATE_90],                         #  8   left     bottom
+]
 
 def resize_image(img_path):
     """"
@@ -24,6 +38,12 @@ def resize_image(img_path):
     if img.size != STANDARD_IMG_SIZE:
         print ("Image size {} differs from standard {}, resizing...".format(
             img.size, STANDARD_IMG_SIZE))
+        try:
+            seq = exif_transpose_sequences[img._getexif()[exif_orientation_tag]]
+        except Exception:
+            pass
+        else:
+            img = functools.reduce(type(img).transpose, seq, img)
         img.thumbnail(STANDARD_IMG_SIZE, Image.ANTIALIAS)
         img.save(img_path, "JPEG")
 
